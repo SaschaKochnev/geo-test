7cb2a66395c493d5ad31105918556c73c521c066
SaschaKochnev <sabha.koch12334@gmail.com>
2026-01-09 23:06:14 +0100

    Change Supabase key in saveLocation function
    Updated Supabase key to a new value.

--- netlify/functions/saveLocation.js
+++ netlify/functions/saveLocation.js
@@ -6,7 +6,7 @@ export async function handler(event) {
     const { latitude, longitude, accuracy } = JSON.parse(event.body);
 
     const SUPABASE_URL = "https://bcejklrgflhaidxbvykz.supabase.co";
-    const SUPABASE_KEY = "sb_publishable_81YR6phB8bHBOFBiudVj-w_qyZR7C_f";
+    const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjZWprbHJnZmxoYWlkeGJ2eWt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5ODU1MTksImV4cCI6MjA4MzU2MTUxOX0.yA0JD_2r_w7o58xV4ZG7cSVKzaMKmyYUIHvObnloTPc";
 
     const res = await fetch(`${SUPABASE_URL}/rest/v1/locations`, {
       method: "POST",
